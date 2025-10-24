f8626beee769dec93ffcbeb2830ad63a065651c5
Victor Miguel Forner <vicforner@gmail.com>
2025-10-24 23:12:14 +0200

    Ex 5 finished

--- 2425S2_EAC1_enunciat/ex6/ex6.js
+++ 2425S2_EAC1_enunciat/ex6/ex6.js
@@ -1,1 +1,14 @@
 // Escriu aquí el teu codi
+let taskList = document.getElementById("taskList");
+let taskInput = document.getElementById("taskInput");
+
+
+function afegirTasca(){
+    let li = document.createElement("li");
+    li.innerHTML = taskInput.textContent;
+    taskInput.append(li);
+}
+
+function esborrarTotes(){
+
+}
