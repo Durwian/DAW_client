e1c0402e1362fa8ee2ac60518c17ac63bb979d86
Victor Miguel Forner <vicforner@gmail.com>
2025-10-28 21:36:00 +0100

    finished ex6

--- ex6/ex6.js
+++ ex6/ex6.js
@@ -1,35 +1,38 @@
 // Escriu aquí el teu codi
-
 let taskInput = document.getElementById("taskInput");
-let taskList = document.getElementById("tasklist");
+let taskList = document.getElementById("taskList");
 let li = document.createElement("li");
-
+console.log(localStorage);
 
 carregarTasques();
 
 function carregarTasques(){
-    for(let i = 0; i < localStorage.length + 1; i++){
-        let item = localStorage.getItem(i);
-        let li = document.createElement('li');
-        li.innerHTML = item;
-        li.append(taskList);
-        console.log(localStorage.getItem(i));
-        
+    let li = document.createElement('li');
+    if(!localStorage){
+        taskList.innerText = "No hi ha tasques pendents";
+    }else if(localStorage.length == 1){
+        taskList.textContent = "";
     }
+    for(let i = 0; i < localStorage.length; i++){
+        let item = localStorage.getItem(i);
+        li.textContent = item;
+        taskList.append(li);
+        console.log("Afegit al localStorage: " + localStorage.getItem(i));
+    }
+    
 }
 
-
-
 function afegirTasca(){
-    if(localStorage.length == null){}
-    localStorage.setItem(localStorage.length,taskInput.value);
-    let li = document.createElement('li');
-    li.innerHTML = taskInput.value;
-    li.append(taskList);
+    if(taskInput.value === ""){
+        return alert("Atenció! No puc posar una tasca buida.");
+    }
+    localStorage.setItem(localStorage.length, taskInput.value);
     taskInput.value = "";
     carregarTasques();
 }
 
 function esborrarTotes(){
     localStorage.clear();
+    taskList.innerHTML = "No hi ha tasques pendents.";
+    console.log("Tots els nodes esborrats.");
 }
