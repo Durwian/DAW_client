441e8edf871e74788d7b1283e0c6418353c1f548
Durwian <vicforner@gmail.com>
2025-10-28 01:40:04 +0100

    finishing

--- ex6/ex6.js
+++ ex6/ex6.js
@@ -1,14 +1,35 @@
 // Escriu aquí el teu codi
-let taskList = document.getElementById("taskList");
+
 let taskInput = document.getElementById("taskInput");
+let taskList = document.getElementById("tasklist");
+let li = document.createElement("li");
+
+
+carregarTasques();
+
+function carregarTasques(){
+    for(let i = 0; i < localStorage.length + 1; i++){
+        let item = localStorage.getItem(i);
+        let li = document.createElement('li');
+        li.innerHTML = item;
+        li.append(taskList);
+        console.log(localStorage.getItem(i));
+        
+    }
+}
+
 
 
 function afegirTasca(){
-    let li = document.createElement("li");
-    li.innerHTML = taskInput.textContent;
-    taskInput.append(li);
+    if(localStorage.length == null){}
+    localStorage.setItem(localStorage.length,taskInput.value);
+    let li = document.createElement('li');
+    li.innerHTML = taskInput.value;
+    li.append(taskList);
+    taskInput.value = "";
+    carregarTasques();
 }
 
 function esborrarTotes(){
-
+    localStorage.clear();
 }
