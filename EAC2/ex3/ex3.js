f0c2c728482ad97d3c4c1e02783c8aed229c552a
Victor Miguel Forner <vicforner@gmail.com>
2025-11-18 22:50:52 +0100

    added EAC2

---
+++ EAC2/ex3/ex3.js
@@ -0,0 +1,4 @@
+const NOMS = ["Laia","Pau","Jana","Nil","Iris","Arnau","Marina","Pol","Aina","Hugo"];
+const COGNOMS = ["Garcia","Fernandez","Lopez","Martinez","Sanchez","Gomez","Perez","Diaz","Ruiz","Hernandez"];
+
+// Escriu aquí el teu codi
